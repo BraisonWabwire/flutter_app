2ea107c0cf78e775a9a0df72e682faab6244178c
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-10-23 04:43:43 +0300

    flutter inherited widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,59 @@
-import 'package:flutter/cupertino.dart';
+// Inherited widget
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyWidget(count: 0));
+  runApp(const MyApp());
 }
 
-class MyWidget extends StatelessWidget {
-  final int count;
-  const MyWidget({super.key, required this.count});
+
+// Defineing inherited widget
+class UserInheritedWidget extends InheritedWidget {
+  final String username;
+
+  const UserInheritedWidget({
+    required this.username,
+    required Widget child, 
+    Key? key,
+  }): super(key: key, child: child);
+
+
+  // Static method to access the inherited widget
+  static UserInheritedWidget? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<UserInheritedWidget>();
+  }
+
+  @override
+  bool updateShouldNotify(covariant UserInheritedWidget oldWidget) {
+    return oldWidget.username != username;
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: const Text('My Widget')),
-        body: Center(
-          child: Text(
-            'Count: $count',
-            style: const TextStyle(fontSize: 24),
-          ),
-        ),
+    return UserInheritedWidget(
+      username: 'Braison Wabwire',
+      child: MaterialApp(
+        home: homePage(),
       ),
     );
   }
 }
+
+class homePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final userWidget = UserInheritedWidget.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inherited Widget Example'),
+      ),
+      body: Center(
+        child: Text('Hello, ${userWidget?.username ?? 'Guest'}!'),
+      ),
+    );
+  }
+}
+
