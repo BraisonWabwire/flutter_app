d03fdc21be45c0bcceff35d71181783b32797468
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-10-23 04:28:57 +0300

    Learning more of stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,23 @@
-import 'dart:async';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyWidget(count: 0));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  // Single subscription stream controller
-  StreamController<String> _streamController = StreamController<String>();
-  // Broadcast stream controller
-  late Stream<String> dataStream;
-  TextEditingController _textEditingController = TextEditingController();
-
-
-  @override
-  void initState() {
-    dataStream = _streamController.stream.asBroadcastStream();
-    super.initState();
-  }
+class MyWidget extends StatelessWidget {
+  final int count;
+  const MyWidget({super.key, required this.count});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text('My App')),
+        appBar: AppBar(title: const Text('My Widget')),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              StreamBuilder<String>(
-                stream: dataStream,
-                builder: (context, asyncSnapshot) {
-                  if (asyncSnapshot.hasData) {
-                    return Text(asyncSnapshot.data!);
-                  } else {
-                    return const Text("No data");
-                  }
-                },
-              ),
-              StreamBuilder<String>(
-                stream: dataStream,
-                builder: (context, asyncSnapshot) {
-                  if (asyncSnapshot.hasData) {
-                    return Text(asyncSnapshot.data!);
-                  } else {
-                    return const Text("No data");
-                  }
-                },
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                width: 200,
-                child: TextField(controller: _textEditingController),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  _streamController.add(_textEditingController.text);
-                },
-                child: Text("Done"),
-              ),
-            ],
+          child: Text(
+            'Count: $count',
+            style: const TextStyle(fontSize: 24),
           ),
         ),
       ),
