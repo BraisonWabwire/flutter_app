c362269af754a3b63bdce0a54ecb837d4a78f4da
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-10-24 14:49:38 +0300

    Stack navigation

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -8,14 +8,18 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Homepage'),
-      ),
+      appBar: AppBar(title: const Text('Homepage')),
       body: Column(
         children: [
-          ElevatedButton(onPressed: (){
-            Navigator.push(context,MaterialPageRoute(builder: (context) => const DetailsPage()));
-          }, child: const Text('Go to Details Page')),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const DetailsPage()),
+              );
+            },
+            child: const Text('Go to Details Page'),
+          ),
         ],
       ),
     );
