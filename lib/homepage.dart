6963657347a9c17ca2a92e703df8a1aa6a0574b1
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-10-24 15:06:17 +0300

    Working with drawer navigation using List View

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,6 +9,40 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Homepage')),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.all(0),
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Text(
+                'Navigation Menu',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 246, 247, 248),
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Home'),
+              onTap: () {
+                Navigator.pop(context); //close drawer
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.info),
+              title: const Text("Details Page"),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const DetailsPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
       body: Column(
         children: [
           ElevatedButton(
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Stack Navigation Demo',
+      title: 'Drawer navigation Demo',
       home: Homepage(),
      
     );
