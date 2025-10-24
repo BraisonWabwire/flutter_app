50409652f173e91929c6b5ed686b7b3664a3e8fc
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-10-24 15:55:12 +0300

    Learning Tabs in flutter

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,60 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/DetailsPage.dart';
+import 'package:flutter_app/settings.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Homepage')),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.all(0),
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue),
-              child: Text(
-                'Navigation Menu',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 246, 247, 248),
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              leading: const Icon(Icons.home),
-              title: const Text('Home'),
-              onTap: () {
-                Navigator.pop(context); //close drawer
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.info),
-              title: const Text("Details Page"),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const DetailsPage()),
-                );
-              },
-            ),
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(title: const Text("Tab Navigation Demo")),
+        body: const TabBarView(children: [DetailsPage(), Settings()]),
+        bottomNavigationBar: const TabBar(
+          tabs: [
+            Tab(icon: Icon(Icons.info), text: 'Info'),
+            Tab(icon: Icon(Icons.settings), text: 'Settings'),
           ],
         ),
-      ),
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const DetailsPage()),
-              );
-            },
-            child: const Text('Go to Details Page'),
-          ),
-        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Drawer navigation Demo',
-      home: Homepage(),
-     
+      home: const Homepage(),
     );
   }
 }
