97daa51ff396184d2205954288a3e8a5abfb411b
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-10-29 18:39:54 +0300

    Read and write file in Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/models/counterModel.dart';
-import 'package:flutter_app/pages/homePage.dart';
-import 'package:provider/provider.dart'; 
+import 'package:flutter_app/pages/homepage.dart';
 
 void main() {
-  runApp(
-      ChangeNotifierProvider(
-      create: (context) => CounterModel(),
-      child: const MyApp(),
-    ),
-  );
+  runApp(const FileReaderApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FileReaderApp extends StatelessWidget {
+  const FileReaderApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Providers demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)
-      ),
-      home: const HomePage(title: "Learning providers"),
+      title: 'File reader example',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Homepage(),
     );
   }
 }
