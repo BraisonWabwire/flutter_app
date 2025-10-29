a8b78ecfa98824c134bb507bf2912bc35e153edf
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-10-29 20:19:53 +0300

    Singelton class in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/pages/homepage.dart';
 
 void main() {
-  runApp(const FileReaderApp());
+  runApp(MyApp());
 }
 
-class FileReaderApp extends StatelessWidget {
-  const FileReaderApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'File reader example',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const Homepage(),
+    return MaterialApp(debugShowCheckedModeBanner: false, home: HomePage(),
+    theme: ThemeData(primaryColor: Colors.blue),
+    );
+  }
+}
+
+class Contact {
+  final String name;
+
+  const Contact({required this.name});
+}
+
+class ContactBook {
+  ContactBook._sharedInstance();
+  static final ContactBook _shared = ContactBook._sharedInstance();
+  factory ContactBook() => _shared;
+
+  final List<Contact> _contacts = [];
+  int get length => _contacts.length;
+
+  void add({required Contact contact}) {
+    _contacts.add(contact);
+  }
+
+  void remove({required Contact contact}) {
+    _contacts.remove(contact);
+  }
+
+  Contact? contact({required int atIndex}) =>
+      _contacts.length > atIndex ? _contacts[atIndex] : null;
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final contactBook = ContactBook();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Home Page"),
+      backgroundColor: Colors.blue,
+      ),
+      body: ListView.builder(
+        itemCount: contactBook.length,
+        itemBuilder: (context, index) {
+          final contact = contactBook.contact(atIndex: index)!;
+          return ListTile(title: Text(contact.name));
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
