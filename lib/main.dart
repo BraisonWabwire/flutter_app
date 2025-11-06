e752a638b1bc96c3119646f2ab8b5f008a7a2bd0
BraisonWabwire <braisonwabwire2003@gmail.com>
2025-11-06 10:57:07 +0300

    Ceated block folders for every feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
-import 'package:flutter_app/data/grocery_data.dart';
-
+import 'package:flutter/material.dart';
+import 'package:flutter_app/features/home/ui/home.dart';
 void main() {
-  var firstProduct = GroceryData.groceryProducts;
+  runApp(MyApp());
+}
 
-  print(firstProduct[5]['description']);
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
 }
